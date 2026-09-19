5853171a3556a587ad2ded5549ca0a9476929c92
Guillaume <73890954+orleyns@users.noreply.github.com>
2026-09-20 01:19:48 +0200

    feat: add initial Color+Hex.swift

---
+++ Incrly/Shared/Color+Hex.swift
@@ -0,0 +1,14 @@
+import SwiftUI
+
+extension Color {
+    init(hex: String) {
+        let cleaned = hex.trimmingCharacters(in: CharacterSet.alphanumerics.inverted)
+        var value: UInt64 = 0
+        Scanner(string: cleaned).scanHexInt64(&value)
+        self.init(
+            red: Double((value >> 16) & 0xFF) / 255,
+            green: Double((value >> 8) & 0xFF) / 255,
+            blue: Double(value & 0xFF) / 255
+        )
+    }
+}
