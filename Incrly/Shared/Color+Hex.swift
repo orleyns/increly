cdf0687cd68a285676245e68214311148884df4b
Guillaume <73890954+orleyns@users.noreply.github.com>
2026-09-20 01:20:03 +0200

    fix: centralize color hex utilities

--- Incrly/Shared/Color+Hex.swift
+++ Incrly/Shared/Color+Hex.swift
@@ -1,4 +1,5 @@
 import SwiftUI
+import UIKit
 
 extension Color {
     init(hex: String) {
@@ -10,5 +11,17 @@ extension Color {
             green: Double((value >> 8) & 0xFF) / 255,
             blue: Double(value & 0xFF) / 255
         )
+    }
+
+    var hexString: String {
+        let uiColor = UIColor(self)
+        var red: CGFloat = 0
+        var green: CGFloat = 0
+        var blue: CGFloat = 0
+        var alpha: CGFloat = 0
+        guard uiColor.getRed(&red, green: &green, blue: &blue, alpha: &alpha) else {
+            return "E9A7F4"
+        }
+        return String(format: "%02X%02X%02X", Int(red * 255), Int(green * 255), Int(blue * 255))
     }
 }
